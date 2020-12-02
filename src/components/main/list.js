5e5cde9291a0614de0c06bed1231e6e1316a95b0
jonathon lee <jonnyleealas@gmail.com>
2020-12-02 16:04:27 -0600

    Update list.js

--- src/components/main/list.js
+++ src/components/main/list.js
@@ -22,7 +22,6 @@ const ListCard = () => [
       >
           <PillPill />
           <Card.Body>
-            <Card.Title>{variant} Card Title </Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up the bulk
                 of the card's content.
