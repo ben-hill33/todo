bcc117c52bca19f34daeb0938a93f6b7b66f0693
jonathon lee <jonnyleealas@gmail.com>
2020-12-02 16:00:43 -0600

    Update pill.js

--- src/components/main/pill.js
+++ src/components/main/pill.js
@@ -1,19 +1,16 @@
 import React from 'react'
-
 import { Badge, Modal, Button } from 'react-bootstrap';
 
 const PillPill = () => {
     return(
     <>
         <div>
-            <Badge pill variant="danger">
-            Complete
-            </Badge>{' '}   
+            <a href="#" class="badge badge-pill badge-danger">Complete</a>
             <button type="button" class="close" aria-label="Close">
             <span aria-hidden="true">&times;</span>
             </button>
         </div>
-        <hr />
+      <hr />
     </>
     )
 } 
